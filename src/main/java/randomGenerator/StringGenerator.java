fbd031bf5e736cb7bd93af969f574a9cbeaf7623
sefaakca <sefaakca0@gmail.com>
2019-09-18 11:36:07 +0100

    Update StringGenerator.java

--- src/main/java/randomGenerator/StringGenerator.java
+++ src/main/java/randomGenerator/StringGenerator.java
@@ -3,6 +3,8 @@ package randomGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringGenerator {
 	
@@ -22,6 +24,41 @@ public class StringGenerator {
 	    }
 	    String generatedString = buffer.toString();
 	    return generatedString;
+	}
+	
+	public String[] stringArrGenerator(String type) 
+	{
+		String[] strArr;
+
+		String pattern ="\\[[^\\d]*(\\d+)[^\\d]*\\]";
+		Pattern p = Pattern.compile(pattern, Pattern.MULTILINE);
+		Matcher _mnum=p.matcher(type);
+		int blen=0;
+		if(_mnum.find()) {
+			String match = _mnum.group();
+			match =match.replace("[", "");
+			match = match.replace("]", "");
+			blen= Integer.parseInt(match);
+			
+			strArr = new String[blen];
+			type=type.substring(0,type.length()-(match.length()+2));
+		}
+		else{
+			strArr= new String[10];
+			type=type.substring(0,type.length()-2);
+		}
+		
+		for(int i =0 ; i<strArr.length;i++) {
+			StringBuilder buffer = new StringBuilder(targetStringLength);
+		    for (int k = 0; k < targetStringLength; k++) {
+		        int randomLimitedInt = hex_down + (int) 
+		          (random.nextFloat() * (hex_up - hex_down + 1));
+		        buffer.append((char) randomLimitedInt);
+		    }
+		    strArr[i] = buffer.toString();
+		}
+		
+	    return strArr;
 	}
 	
 
